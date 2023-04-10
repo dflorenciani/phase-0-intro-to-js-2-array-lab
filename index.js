e9ade2a0da7d2ac7dd50903ee22f0c3957f1c310
Didiel <dflorenciani@gmail.com>
2023-04-10 09:58:38 -0500

    newsubmission

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
+
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph")
+
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob")
+
+}
+function destructivelyRemoveLastCat(name) {
+    return cats.pop("Ralph") 
+
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift("Bob") 
+
+
+}
+appendCat("Broom")
+
+function appendCat(name){
+    const cats = ["Milo", "Otis", "Garfield"];
+    const newCats = [...cats,name]
+    return newCats;
+
+
+}
+PrependCat("Arnold")
+
+function prependCat(name){
+    const cats = ["Milo", "Otis", "Garfield"]
+    const newCats = [name,...cats]
+    return newCats;
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length -1);
+    
+
+}
+function removeFirstCat() {
+    return cats.slice(1)
+
+}
+
+
+    
+
+
+
+
+
+
+
+
